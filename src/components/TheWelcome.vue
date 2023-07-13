b3e6fbaed0a84c3bea73fbbc824006782c985a13
Ryan Eastman <rking1323@gmail.com>
2023-07-12 19:22:45 -0700

    added meet pat

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,11 +12,26 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>Meet Pat!</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    Pat is a 43-year-old fisherman who has dedicated his entire life to the fishing industry. He was born and raised in a small coastal town and spent most of his childhood on the water. Pat's passion for fishing began at an early age when he would go out on his father's boat and help with the family's commercial fishing business.
+        
+    Pat is a 43-year-old fisherman who has dedicated his entire life to the fishing industry. He was born and raised in a small coastal town and spent most of his childhood on the water. Pat's passion for fishing began at an early age when he would go out on his father's boat and help with the family's commercial fishing business.
+    
+    As he grew older, Pat became more interested in the sustainability of the fish populations he was catching. He realized that overfishing was a serious threat to the environment, and he wanted to do his part to help preserve it. Pat began working with local conservation organizations to promote responsible fishing practices and to raise awareness about the importance of protecting the ocean's resources.
+          
+    After many years of hard work and dedication, Pat decided to take his passion for sustainable fishing to the next level. He opened his own bakery, where he combined his love for sweets with his commitment to the environment. Pat's bakery specializes in using only sustainably sourced ingredients, including fish caught using responsible fishing practices.
+          
+    Pat's bakery has become a popular destination for locals and tourists alike. His customers love the unique and delicious flavors he creates, but they also appreciate the fact that they are supporting a business that is committed to protecting the environment. Pat's bakery has become a symbol of what is possible when you combine your passion with your values.
+          
+    Pat is a devoted family man, married to his wife Lindsey for over 20 years. They have two children, Sam and Rebecca, who are both proud of their father's accomplishments. Pat's family is his biggest source of inspiration and support. They have been with him every step of the way as he pursued his dreams.
+          
+    Despite his success, Pat has never forgotten his roots. He still goes out on his boat whenever he can, and he always makes sure to follow responsible fishing practices. Pat's commitment to sustainability and his love for the ocean continue to guide him in everything he does, both in his bakery and in his personal life.
+          
+    In addition to his work with his bakery, Pat is also actively involved in his local community. He volunteers with several organizations that promote sustainable fishing and ocean conservation. He also helps mentor young people who are interested in pursuing careers in the fishing industry, passing on his knowledge and passion to the next generation.
+          
+    Pat's story is an inspiring one. He has dedicated his life to his passions, his family, and his community. Through his work with his bakery and his commitment to sustainable fishing practices, he has become a true leader in his field. Pat's legacy will undoubtedly be felt for many years to come, both in his local community and in the wider world.
+
   </WelcomeItem>
 
   <WelcomeItem>
